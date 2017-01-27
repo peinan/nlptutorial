82f4b03224f7d6eca512c0c3505b7931fffbb028
Peinan ZHANG <pzhang@yahoo-corp.jp>
2017-01-27 17:05:04 +0900

    :heavy_plus_sign: added chap01 codes

---
+++ code/01-unigramlm/test-01-unigramlm.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# Author: Peinan ZHANG
+# Created at: 2017-01-27
+
+import sys, cPickle as pkl, math
+from collections import defaultdict
+
+def load_unigram_lm(filepath, mode='plain'):
+  unigram_lm = {}
+  if mode == 'plain':
+    for line in open(filepath):
+      line = line.rstrip('\n')
+      word, prob = line.split('\t')
+      unigram_lm[word] = float(prob)
+  elif mode == 'pickle':
+    unigram_lm = pkl.load(open(filepath))
+
+  return unigram_lm
+
+
+def eval_model(filepath, lm, lambda_unk=.05, N=100000):
+  """Evaluate the test dataset. This function returns entropy and coverage.
+
+  Probability:
+    P(w_i) = (1 - lambda_unk) * P_ML(w_i) + lambda_unk * 1/N
+  Log Likelihood:
+    log P(W_test | M) = sigma ( log P(w | M) )
+  Entropy:
+    H(W_test | M) = 1/|W_test| sigma ( -log2 P(w | M) )
+  Perplexity:
+    PPL = 2^H
+  Coverage:
+    The rate of the words appeared in the test dataset that are covered in the 
+    language model.
+  """
+
+  total_word_count = 0
+  not_found_word_map = defaultdict(int)
+  H = 0
+  for line in open(filepath):
+    line = line.rstrip('\n') + ' </s>'
+    for word in line.split(' '):
+      try:
+        P = (1 - lambda_unk) * lm[word] + lambda_unk / float(N)
+      except KeyError:
+        P = lambda_unk / float(N)
+        not_found_word_map[word] += 1
+      total_word_count += 1
+      H -= math.log(P, 2)
+  
+  H = H/float(total_word_count)
+  lm_keys = len(lm.keys())
+  coverage = (lm_keys - len(not_found_word_map.keys())) / float(lm_keys)
+
+  return H, coverage
+
+
+def main():
+  lm = load_unigram_lm(sys.argv[1])
+  H, coverage = eval_model(sys.argv[2], lm)
+  print("Entropy: %f, Coverage: %s" % (H, coverage))
+
+
+if __name__ == '__main__':
+  main()
---
+++ code/01-unigramlm/train-01-unigramlm.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# Author: Peinan ZHANG
+# Created at: 2017-01-27
+
+import sys, cPickle as pkl
+from collections import defaultdict
+
+def load_train_file(filepath):
+  word_count_map   = defaultdict(int)
+  total_word_count = 0
+  for line in open(filepath):
+    line = line.rstrip('\n') + ' </s>'
+    for word in line.split(' '):
+      word_count_map[word] += 1
+      total_word_count += 1
+
+  return word_count_map, total_word_count
+
+
+def make_unigram_lm(word_count_map, total_word_count):
+  """Make an unigram language model
+  P(w_i) = word_count / total_word_count
+  """
+  unigram_lm = {}
+  for word, word_count in word_count_map.items():
+    unigram_lm[word] = word_count / float(total_word_count)
+
+  return unigram_lm
+
+
+def write_lm(lm, filepath, mode='plain'):
+  if mode == 'plain':
+    with open(filepath, 'w') as outfile:
+      for word, p in lm.items():
+        outfile.write("%s\t%.10f\n" % (word, p))
+  elif mode == 'pickle':
+    pkl.dump(lm, open(filepath, 'w'))
+
+  print("Successfully write language model to '%s'." % filepath)
+
+
+def print_dict(target_dict, sort=True, reverse=True, max_item=10):
+  items = target_dict.items()
+  len_keys = len(target_dict.keys())
+  if sort:
+    items = sorted(target_dict.items(), key=lambda x:x[1], reverse=reverse)
+  if not max_item:
+    max_item = len_keys
+  items = items[:max_item]
+
+  for k, v in items:
+    print(k, v)
+  print("Total key count:", len_keys)
+
+
+def main():
+  word_count_map, total_word_count = load_train_file(sys.argv[1])
+  lm = make_unigram_lm(word_count_map, total_word_count)
+
+  print("Word count map")
+  print_dict(word_count_map)
+  print("Total word count:", total_word_count)
+  print("Language model")
+  print_dict(lm)
+
+  write_lm(lm, sys.argv[2])
+
+
+if __name__ == '__main__':
+  main()
